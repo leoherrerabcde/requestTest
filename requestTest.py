a6dbc554aad9c8980237481b14ff7778e19ce731
LeonardoHerrera <leo.herrera.bcde@gmail.com>
2019-02-01 20:45:39 -0300

    socket

--- requestTest.py
+++ requestTest.py
@@ -1,5 +1,6 @@
 
 import requests
+import socket
 
 url = "http://192.168.8.102:3000/api/areas"
 
@@ -10,3 +11,11 @@ r = requests.get(url)
 
 print "The get request response is:"
 print (r.text)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host_ip = socket.gethostbyname('127.0.0.1') 
+s.connect((host_ip, 4000))
+
+
+print "the socket has successfully connected on port == %s" %(host_ip)
